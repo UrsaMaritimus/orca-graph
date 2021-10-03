357b47bf7f4a37e312a2acb62f461c646e33d2ba
Ursa M <maritim.ursa@gmail.com>
2021-10-02 23:10:35 -0700

    Save fuji deploy update

--- src/orca.ts
+++ src/orca.ts
@@ -19,9 +19,11 @@ export function handleTransfer(event: Transfer): void {
   let orca = Orca.load(event.address.toHexString());
   if (orca === null) {
     orca = new Orca(event.address.toHexString());
-    orca.circulatingSupply = BigInt.fromI32(0);
+    orca.circulatingSupply = BigInt.fromString('25000000000000000000000');
     orca.maxSupply = BigInt.fromI32(0);
-    orca.treasury = Bytes.fromHexString(to) as Bytes;
+    orca.treasury = Bytes.fromHexString(
+      '0x08eaea50d8f5d423f6904246c7b5f05f9bdb3957'
+    ) as Bytes;
     orca.team = Bytes.fromHexString(ADDRESS_TEAM) as Bytes;
     orca.leader = Bytes.fromHexString(POD_LEADER_ADDRESS) as Bytes;
     orca.save();
@@ -30,13 +32,15 @@ export function handleTransfer(event: Transfer): void {
   if (from == ADDRESS_ZERO) {
     orca.maxSupply = amount;
   } else if (
-    to == orca.team.toHexString() ||
-    to == orca.leader.toHexString() ||
-    to == orca.treasury.toHexString()
+    from == '0x318dfbe56155f9999fa913cddcaa5764a2b52134' ||
+    from == '0x4422fb9afb547e8ed7a61ac9de0255c760ea55c1' ||
+    from == '0x111e1e97435b57467e79d4930acc4b7eb3d478ad' ||
+    from == '0x8b1d98a91f853218ddbb066f20b8c63e782e2430' ||
+    from == '0x4fb25469d273225e2b777d80e27754776ecbcee5'
   ) {
-    return;
-  } else {
     orca.circulatingSupply = orca.circulatingSupply.plus(amount);
+  } else {
+    return;
   }
 
   orca.save();
